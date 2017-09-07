81dd3eae489ee5b00ffa991b00aed3232f2d5f48
arkan382382 <arkan382382@outlook.com>
2017-09-07 20:06:53 +0200

    zapis do pliku :D

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,8 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.lang.management.BufferPoolMXBean;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,16 +19,42 @@ public class MainClass {
 
         System.out.println(small.getNumberOfLoadedPictures());
 
-        FinalImage f = new FinalImage();
-        BufferedImage tmp2 = new BufferedImage(10, 10, BufferedImage.TYPE_INT_RGB);
-        tmp2 = f.CreateFinalWorkingArea();
+            FinalImage f = new FinalImage();
+            BufferedImage tmp2 = new BufferedImage(10, 10, BufferedImage.TYPE_INT_RGB);
+            tmp2 = f.createFinalWorkingArea();
         System.out.println(tmp2.getWidth() + " " + tmp2.getHeight());
 
         System.out.println(f.cellsHorizontally + " " + f.cellsVertically);
         f.checkNumberOfSocketsInRowsAndColumns(small.getNumberOfLoadedPictures(), small);
         System.out.println(f.cellsHorizontally + " " + f.cellsVertically);
+            Shower show = new Shower();
+            show.savePicture();
 
+            // test zapisu
+        BufferedImage[] socket = new BufferedImage[small.getNumberOfLoadedPictures()];
+        int positionX = 0;
+        int positionY = 0;
 
-
+        for(int i=0; i<small.getNumberOfLoadedPictures(); i++){
+            socket[i] = small.images.get(i);
+        }
+        BufferedImage result = f.createFinalWorkingArea();
+        Graphics g = result.getGraphics();
+        int licznik = 0;
+        for(int i=0; i<small.getNumberOfLoadedPictures(); i++){
+            g.drawImage(socket[i], positionX, positionY, null);
+            //change positionX and positionY with values after scaling
+            positionX = positionX + small.images.get(i).getWidth();
+            licznik = f.cellsHorizontally;
+            if((i % licznik) == 0 && i != 0){             //(i%(f.cellsHorizontally-1))==0)   (i==(f.cellsHorizontally-1))
+                positionX = 0;
+                positionY = positionY + small.images.get(i).getHeight();
+            }
+           // positionY = positionY + small.images.get(i).getHeight();
+           // if(i==(f.cellsVertically-1)){
+           //     positionY = 0;
+           // }
+        }
+        ImageIO.write(result, "jpg", new File("D:\\zrodlo\\result.jpg"));
     }
 }
